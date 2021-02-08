c62ddff7b0f47923e8729516081118a73900f1c0
Andy Grauch <andy.grauch@sovos.com>
2021-02-08 10:39:55 -0500

    add graph

---
+++ dataScripts/buildGscData.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+const parse = require('csv-parse');
+
+function findTreeNode(id, node) {
+  if (node.id === id) {
+    return node;
+  }
+
+  if (node.children && node.children.length) {
+    for (let i = 0; i < node.children.length; i++) {
+      const found = findTreeNode(id, node.children[i]);
+      if (found) {
+        return found;
+      }
+    }
+  }
+
+  return undefined;
+}
+
+async function buildTree() {
+  const tree = {
+    id: '0',
+    name: 'Codes',
+  };
+  const gscData = fs
+    .createReadStream(path.join(__dirname, 'GSC_E_U 11_01_20.csv'))
+    .pipe(parse({ columns: true }));
+
+  for await (const row of gscData) {
+    const parent = findTreeNode(row.PARENT_ID, tree);
+
+    if (!parent) {
+      console.log(row);
+      continue;
+    }
+
+    if (!parent.children) {
+      parent.children = [];
+    }
+
+    parent.children.push({
+      id: row.ID,
+      name: row.GS_CODE,
+      attributes: {
+        Description: row.DESCRIPTION,
+      },
+    });
+  }
+
+  return tree;
+}
+
+async function writeGscData() {
+  const data = await buildTree();
+  fs.writeFileSync(
+    path.resolve(__dirname, '../src/50data/gsc.json'),
+    JSON.stringify(data)
+  );
+}
+
+(async () => await writeGscData())();
--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Tree from 'react-d3-tree';
+import gscData from './data/gsc.json';
 
+const renderNode = ({ nodeDatum, toggleNode }) => {
+  console.log(nodeDatum);
+  return (
+    <g title={nodeDatum.attributes && nodeDatum.attributes.Description}>
+      <circle
+        r={15}
+        fill={nodeDatum.children ? '666' : '  white'}
+        onClick={nodeDatum.children ? toggleNode : undefined}
+      />
+      <foreignObject height={50} width={100} y={20}>
+        <div
+          style={{ fontSize: '.67em', fontWeight: 'bold' }}
+          title={nodeDatum.attributes && nodeDatum.attributes.Description}
+        >
+          {nodeDatum.name}
+        </div>
+      </foreignObject>
+    </g>
+  );
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{ border: '1px solid black', height: 750, margin: 30, width: 750 }}
+    >
+      <Tree
+        data={gscData}
+        collapsible={true}
+        initialDepth={2}
+        renderCustomNodeElement={renderNode}
+        translate={{ x: 50, y: 375 }}
+        zoom={0.2}
+      />
     </div>
   );
 }
